c44b70f2800ea02da49befc9d0dad196b7abdf92
n286668-sgt_sangroup <javier.payno@gruposantander.com>
2023-05-31 10:51:07 +0200

    Initial upload

---
+++ main.tf
@@ -0,0 +1,31 @@
+resource "aws_db_instance" "default" {
+  identifier           = "bbdd"
+  allocated_storage    = 10
+  db_name              = "mydb"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+  # snapshot_identifier  = aws_db_snapshot.test.id
+}
+
+resource "aws_db_snapshot" "test" {
+  db_instance_identifier = aws_db_instance.default.id
+  db_snapshot_identifier = "testsnapshot1234"
+}
+
+resource "aws_db_instance" "restore" {
+  allocated_storage    = 10
+  db_name              = "mydb"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+  snapshot_identifier  = aws_db_snapshot.test.id
+}
