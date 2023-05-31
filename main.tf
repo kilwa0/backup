6cc14c71a6d89b1c2b44aed50911711219dd2b41
n286668-sgt_sangroup <javier.payno@gruposantander.com>
2023-05-31 11:45:44 +0200

    delete restore resource

--- main.tf
+++ main.tf
@@ -17,15 +17,15 @@ resource "aws_db_snapshot" "test" {
   db_snapshot_identifier = "testsnapshot1234"
 }
 
-resource "aws_db_instance" "restore" {
-  allocated_storage    = 10
-  db_name              = "mydb"
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t3.micro"
-  username             = "foo"
-  password             = "foobarbaz"
-  parameter_group_name = "default.mysql5.7"
-  skip_final_snapshot  = true
-  snapshot_identifier  = aws_db_snapshot.test.id
-}
+# resource "aws_db_instance" "restore" {
+#   allocated_storage    = 10
+#   db_name              = "mydb"
+#   engine               = "mysql"
+#   engine_version       = "5.7"
+#   instance_class       = "db.t3.micro"
+#   username             = "foo"
+#   password             = "foobarbaz"
+#   parameter_group_name = "default.mysql5.7"
+#   skip_final_snapshot  = true
+#   snapshot_identifier  = aws_db_snapshot.test.id
+# }
