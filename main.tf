aa180316dc163ec8a997f938921bf0ba6a3c0a3f
n286668-sgt_sangroup <javier.payno@gruposantander.com>
2023-05-31 12:04:17 +0200

    change db id

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "aws_db_instance" "default" {
 }
 
 resource "aws_db_snapshot" "test" {
-  db_instance_identifier = aws_db_instance.default.id
+  db_instance_identifier = aws_db_instance.default.identifier
   db_snapshot_identifier = "testsnapshot1234"
 }
 
